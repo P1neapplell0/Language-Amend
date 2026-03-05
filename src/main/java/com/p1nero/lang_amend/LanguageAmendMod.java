bacdf80dccfe4b714ea304b189309f0a8ac0608f
p1nero <1070402601@qq.com>
2026-03-05 23:14:19 +0800

    port to 1.21.1

--- src/main/java/com/p1nero/lang_amend/LanguageAmendMod.java
+++ src/main/java/com/p1nero/lang_amend/LanguageAmendMod.java
@@ -1,7 +1,7 @@
 package com.p1nero.lang_amend;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
 @Mod(LanguageAmendMod.MOD_ID)
